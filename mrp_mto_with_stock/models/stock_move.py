f00612a870a7ad6b01410b34dca8313317012e2e
Chafique <chafique.delli@akretion.com>
2019-07-08 17:59:36 +0200

    [FIX] unit_factor is not update when split the moves of the MO

---
+++ mrp_mto_with_stock/models/stock_move.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, api
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    @api.multi
+    def _prepare_move_split_vals(self, defaults):
+        defaults = super(StockMove, self)._prepare_move_split_vals(defaults)
+        quantity = defaults['product_uom_qty']
+        production = self.raw_material_production_id
+        factor = quantity / (production.product_qty - production.qty_produced)
+        defaults['unit_factor'] = factor
+        return defaults
+
+    @api.multi
+    def split(self, qty, restrict_lot_id=False, restrict_partner_id=False):
+        res = super(StockMove, self).split(
+            qty=qty, restrict_lot_id=restrict_lot_id,
+            restrict_partner_id=restrict_partner_id)
+        production = self.raw_material_production_id
+        self.unit_factor = self.product_uom_qty / (
+            production.product_qty - production.qty_produced)
+        return res
