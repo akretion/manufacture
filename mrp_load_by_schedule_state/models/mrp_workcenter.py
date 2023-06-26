3f6153aad4a96206e3cffbb8558db2185e3c94b2
Florian da Costa <florian.dacosta@akretion.com>
2023-06-26 11:58:48 +0200

    [FIX] Recompute load fields ad read_group and search_read from UI This way the load values should always be ok when looked from Odoo UI, which is their only purpose

--- mrp_load_by_schedule_state/models/mrp_workcenter.py
+++ mrp_load_by_schedule_state/models/mrp_workcenter.py
@@ -1,6 +1,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class MrpWorkcenter(models.Model):
@@ -30,6 +30,13 @@ class MrpWorkcenter(models.Model):
     # as well when it already computes it for all other fields (performance)
     workcenter_load = fields.Float(compute="_compute_load", store=True)
 
+    # it is stored only because it is mandatory, in order to get the sum on the views
+    # We used to compute when user click on the menu, but now it is cached and
+    # overriding the load controller does not work.
+    # I choose for now to compute it overriding the web_search_read/read_group
+    # as it is really something needed only for the tree view.
+    # maybe it would be better to put the right api.depends, but I fear it
+    # recompute a lot and add a lot of conccurent update in widly used database.
     def _compute_load(self):
         res = self.env["mrp.workorder"].read_group(
             [
@@ -70,3 +77,54 @@ class MrpWorkcenter(models.Model):
             workcenter.workcenter_load = sum(
                 load for _schedule_state, load in result[workcenter.id]["time"].items()
             )
+
+    @api.model
+    def web_read_group(
+        self,
+        domain,
+        fields,
+        groupby,
+        limit=None,
+        offset=0,
+        orderby=False,
+        lazy=True,
+        expand=False,
+        expand_limit=None,
+        expand_orderby=False,
+    ):
+        self.search(domain, offset=offset, limit=limit, order=orderby)._compute_load()
+        return super().web_read_group(
+            domain,
+            fields,
+            groupby,
+            limit=limit,
+            offset=offset,
+            orderby=orderby,
+            lazy=lazy,
+            expand=expand,
+            expand_limit=expand_limit,
+            expand_orderby=False,
+        )
+
+    @api.model
+    def web_search_read(
+        self,
+        domain=None,
+        fields=None,
+        offset=0,
+        limit=None,
+        order=None,
+        count_limit=None,
+    ):
+        search_domain = domain or []
+        self.search(
+            search_domain, offset=offset, limit=limit, order=order
+        )._compute_load()
+        return super().web_search_read(
+            domain=domain,
+            fields=fields,
+            offset=offset,
+            limit=limit,
+            order=order,
+            count_limit=count_limit,
+        )
