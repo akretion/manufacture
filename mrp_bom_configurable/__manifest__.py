3be381b6532fc6d3518a79c07988eb8c2ffde915
Francois Poizat <francois.poizat@gmail.com>
2023-08-16 17:05:57 +0200

    Merge 16-input-conf and 16-variable-bom

---
+++ mrp_bom_configurable/__manifest__.py
@@ -0,0 +1,30 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "MRP Bom Variable",
+    "summary": "Skip components lines in bom according to conditions",
+    "version": "16.0.1.0.0",
+    "category": "Manufacture",
+    "website": "https://github.com/OCA/manufacture",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp",
+    ],
+    "maintainer": [
+        "bealdav",
+    ],
+    "data": [
+        "views/input_config.xml",
+        "views/input_line.xml",
+        "views/mrp_view.xml",
+        "report/report.xml",
+        "report/bom_variable.xml",
+        "security/ir.model.access.csv",
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'mrp_bom_variable/static/src/**/*'
+        ]
+    },
+    "installable": True,
+}
---
+++ mrp_bom_configurable/report/bom_structure.py
@@ -0,0 +1,50 @@
+from odoo import api, models
+from odoo import fields, models, api
+
+class ReportBomStructure(models.AbstractModel):
+    _name = 'mrp_bom_variable.report.mrp.report_bom_structure'
+    _inherit = 'report.mrp.report_bom_structure'
+
+    @api.model
+    def _get_bom_data(self, *args, **kwargs):
+        bom_report_line = super(ReportBomStructure, self)._get_bom_data(*args, **kwargs)
+        bom_line = kwargs.get('bom_line', False)
+
+        bom_report_line['domain'] = bom_line.domain if bom_line else False
+
+        return bom_report_line
+
+    @api.model
+    def _get_operation_line(self, *args, **kwargs):
+        operations = super(ReportBomStructure, self)._get_operation_line(*args, **kwargs)
+        bom = args[1]
+
+        index = 0
+        for operation in bom.operation_ids:
+            if not product or operation._skip_operation_line(product):
+                continue
+            operations[index]['domain'] = operation.domain
+            index += 1
+
+        return operations
+
+    @api.model
+    def _get_byproducts_lines(self, *args, **kwargs):
+        byproducts = super(ReportBomStructure, self)._get_byproducts_lines(*args, **kwargs)
+        bom = args[1]
+
+        index = 0
+        for byproduct in bom.byproduct_ids:
+            if byproduct._skip_byproduct_line(product):
+                continue
+            byproducts[index]['domain'] = byproduct.domain
+
+        return byproducts
+
+    @api.model
+    def _get_component_data(self, *args, **kwargs):
+        component_data = super(ReportBomStructure, self)._get_component_data(*args, **kwargs)
+        bom_line = args[2]
+
+        component_data['domain'] = bom_line.domain if bom_line else False
+        return component_data
