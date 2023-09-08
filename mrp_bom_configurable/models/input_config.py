6ad2c974229a33d1e86838c41ed5e8edc1cfa112
David Beal <david.beal@akretion.com>
2023-09-08 17:06:34 +0200

    IMP mrp_bom_conf: bom_id domain

--- mrp_bom_configurable/models/input_config.py
+++ mrp_bom_configurable/models/input_config.py
@@ -6,11 +6,36 @@ class InputConfig(models.Model):
     _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = "Product configuration scenari"
 
+    def _compute_bom_domain(self):
+        var_boms = self.env["mrp.bom"].search([("configuration_type", "=", "variable")])
+        var_tmpl = var_boms.mapped("product_tmpl_id")
+        products = self.env["product.product"].search(
+            [("product_tmpl_id", "in", var_tmpl.ids)]
+        )
+        compon_tmpl = (
+            self.env["mrp.bom.line"]
+            .search([("product_id", "=", products.ids)])
+            .mapped("product_id.product_tmpl_id")
+        )
+        tmpls = var_tmpl - compon_tmpl
+        boms = self.env["mrp.bom"].search(
+            [
+                ("configuration_type", "=", "variable"),
+                ("product_tmpl_id", "in", tmpls.ids),
+            ]
+        )
+        for rec in self:
+            rec.bom_domain = [("id", "in", boms.ids)]
+
     name = fields.Char(required=True)
-    date = fields.Date(string="Date", default=fields.Date.today())
+    date = fields.Date(default=fields.Date.today())
     bom_id = fields.Many2one(
-        comodel_name="mrp.bom", string="Configurable Product", required=True
+        comodel_name="mrp.bom",
+        string="Configurable Product",
+        required=True,
+        domain=_compute_bom_domain,
     )
+    bom_domain = fields.Binary(compute="_compute_bom_domain")
     state = fields.Selection(
         selection=[
             ("draft", "Draft"),
