e7d586597e93d40550b7a37b380f7976ea99c5dc
hparfr <raphael.reverdy@akretion.com>
2025-06-12 22:25:41 +0200

    [MIG] mrp_bom_line_description: Migration to 18.0

--- mrp_bom_line_description/__manifest__.py
+++ mrp_bom_line_description/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Mrp Bom Line Description",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Manufacturing",
     "author": "Akretion,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/manufacture",
